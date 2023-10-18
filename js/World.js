a2840e003d5ab789153576e670bda0823406de05
Pat <kovacs.patrik29@gmail.com>
2023-10-18 23:32:07 +0200

    Updated

--- js/World.js
+++ js/World.js
@@ -7,33 +7,53 @@ export class World{
     constructor(){
         this.lastTick = Date.now();
         this.world = new p2.World({gravity: [0, 0]});
-        this.falFelso = new p2.Body();
-        this.falFelsoShape = new p2.Plane({material: Materials.falMaterial});
-        this.falBal = new p2.Body();
-        this.falBalShape = new p2.Plane({material: Materials.falMaterial});
-        this.falJobb = new p2.Body();
-        this.falJobbShape = new p2.Plane({material: Materials.falMaterial});
-        this.falAlso = new p2.Body();
-        this.falAlsoShape = new p2.Plane({material: Materials.falMaterial});
-
         this.palyaSzelesseg = 1000;
         this.palyaMagassag = 800;
+        this.kapuSzelesseg = 200;
+        this.verticalFalHosszusag = (this.palyaMagassag - this.kapuSzelesseg)/2
         this.balSzel = window.innerWidth/2-this.palyaSzelesseg/2;
         this.jobbSzel = window.innerWidth/2+this.palyaSzelesseg/2;
         this.alSzel = window.innerHeight/2+this.palyaMagassag/2;
         this.felSzel = window.innerHeight/2-this.palyaMagassag/2;
+        this.falFelso = new p2.Body();
+        this.falFelsoShape = new p2.Box({material: Materials.falMaterial, width: this.palyaSzelesseg+30, height: 30});
+
+        this.falBalFelso = new p2.Body();
+        this.falBalFelsoShape = new p2.Box({material: Materials.falMaterial, width: 30, height: this.verticalFalHosszusag});
+        this.falBalAlso = new p2.Body();
+        this.falBalAlsoShape = new p2.Box({material: Materials.falMaterial, width: 30, height: this.verticalFalHosszusag});
+
+        this.falJobb = new p2.Body();
+        this.falJobbShape = new p2.Box({material: Materials.falMaterial, width: 30, height: this.palyaMagassag+30});
+        
+        this.falAlso = new p2.Body();
+        this.falAlsoShape = new p2.Box({material: Materials.falMaterial, width: this.palyaSzelesseg+30, height: 30});
+        this.falFelso.position = [this.balSzel+this.palyaSzelesseg/2, this.felSzel-10];
+
+        this.falBalFelso.position = [this.balSzel-10, this.felSzel+this.verticalFalHosszusag/2];
+        this.falBalAlso.position = [this.balSzel-10, this.alSzel-this.verticalFalHosszusag/2];
+
+        this.falJobb.position = [this.jobbSzel+10, this.felSzel+this.palyaMagassag/2];
+        this.falAlso.position = [this.balSzel+this.palyaSzelesseg/2, this.alSzel+10];
+        
 
 
 
         this.world.addContactMaterial(new p2.ContactMaterial(Materials.playerMaterial, Materials.ballMaterial, {friction: 500, restitution: 0}));
         this.world.addContactMaterial(new p2.ContactMaterial(Materials.playerMaterial, Materials.falMaterial, {friction: 0, stiffness: Number.POSITIVE_INFINITY}));
-        this.falFelso.addShape(this.falFelsoShape, [this.balSzel, this.felSzel], 0);
-        this.falBal.addShape(this.falBalShape, [this.balSzel, this.felSzel], -Math.PI/2);
-        this.falJobb.addShape(this.falJobbShape, [this.jobbSzel, this.alSzel], Math.PI/2);
-        this.falAlso.addShape(this.falAlsoShape, [this.jobbSzel, this.alSzel], Math.PI);
+        this.falFelso.addShape(this.falFelsoShape, [0, 0], 0);
+
+        this.falBalFelso.addShape(this.falBalFelsoShape, [0, 0], 0);
+        this.falBalAlso.addShape(this.falBalAlsoShape, [0, 0], 0);
+
+        this.falJobb.addShape(this.falJobbShape, [0, 0], 0);
+        this.falAlso.addShape(this.falAlsoShape, [0, 0], 0);
         
         this.world.addBody(this.falFelso);
-        this.world.addBody(this.falBal);
+
+        this.world.addBody(this.falBalFelso);
+        this.world.addBody(this.falBalAlso);
+
         this.world.addBody(this.falJobb);
         this.world.addBody(this.falAlso);
 
