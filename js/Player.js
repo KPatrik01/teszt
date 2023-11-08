f073a92cff02d1d38cd6d8061a77cb80e4f307f6
VARD <veressandor03@gmail.com>
2023-11-08 11:37:24 +0100

    Stamina

--- js/Player.js
+++ js/Player.js
@@ -5,18 +5,29 @@ import collisionGroups from "./CollisionGroups.js";
 export class Player{
     constructor(){
         this.radius = 22;
-        this.speed = 200;
+        this.speed = 250;
+        this.stamina = 100;
         this.kick = false;
         this.body = new p2.Body({mass: 1, damping: 0, angularDamping: 0, fixedRotation: true});
         this.shape = new p2.Circle({radius: this.radius, material: Materials.playerMaterial, collisionGroup: collisionGroups.player, collisionMask: collisionGroups.ball | collisionGroups.fal});
         this.body.addShape(this.shape);
-        this.body.position = [1700, 1850];
+        this.body.position = [1660, 2250];
     }
     processInput(){
         if ( this.input.sprint == true){
-            this.speed=480;
+            if (this.stamina >= 1) {
+                this.speed=450;
+                this.stamina = Math.max(0,this.stamina-1);
+                console.log(this.stamina);
+            } else if (this.stamina < 1) {
+                this.speed = 250
+                this.stamina = Math.min(100,this.stamina+0.25);
+                console.log(this.stamina)
+            }
         } else {
             this.speed=250;
+            this.stamina = Math.min(100,this.stamina+0.75);
+            console.log(this.stamina)
         }
         this.kick = this.input.kick
         if (this.input.move){
--- js/PlayerShape.js
+++ js/PlayerShape.js
@@ -4,8 +4,10 @@ export class PlayerShape{
         this.container = new PIXI.Container();
         this.playerGraphics = new PIXI.Graphics();
         this.kickGraphics = new PIXI.Graphics();
+        this.staminaGraphics = new PIXI.Graphics();
         this.container.addChild(this.kickGraphics);
         this.container.addChild(this.playerGraphics);
+        this.container.addChild(this.staminaGraphics);
         this.redraw();
     }
     redraw(){
@@ -20,5 +22,12 @@ export class PlayerShape{
         this.container.position.x=this.player.body.position[0];
         this.container.position.y=this.player.body.position[1];
         this.kickGraphics.visible = this.player.kick;
+        this.drawStamina();
+    }
+    drawStamina(){
+        this.staminaGraphics.clear()
+        this.staminaGraphics.beginFill(0x006600,);
+        this.staminaGraphics.drawRect(-20,30,this.player.stamina/2.5,5);
+        this.staminaGraphics.endFill();
     }
 }
